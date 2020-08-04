3d8bca7486197db078fc0e05795d3a10d4e4ce8e
Arek Cios <arek123113@gmail.com>
2020-08-04 20:50:49 +0200

    InitialCommit

---
+++ Bot.py
@@ -0,0 +1,35 @@
+import lyricsgenius
+from random import randrange
+import time
+import tweepy
+
+genius = lyricsgenius.Genius("")
+genius.remove_section_headers = True
+artist = genius.search_artist("artist", max_songs=999, sort="title")
+
+auth = tweepy.OAuthHandler("", "")
+auth.set_access_token("", "")
+api = tweepy.API(auth)
+
+while True:
+    try:
+        #GENIUS API PART
+        song_title = artist.songs[randrange(artist.num_songs+1)].title
+        print(song_title)
+        song = genius.search_song(song_title, artist.name)
+        numberoflines = len(song.lyrics.splitlines())
+        barLine = randrange(numberoflines-1)
+
+        #TEST
+        print (song.lyrics.splitlines()[barLine])
+        print (song.lyrics.splitlines()[barLine+1])
+        print(barLine)
+        print(numberoflines)
+
+        #TWEET IT
+        tweet_message = song.lyrics.splitlines()[barLine] + ' ' + song.lyrics.splitlines()[barLine+1] + ' ~ Lil Tracy'
+        api.update_status(tweet_message)
+
+        time.sleep(120)
+    finally:
+       continue
